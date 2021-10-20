b140a3fc9320911480f5ef49103b5fae4facdedb
Alexey Kovalenko <a.kovalenko@nimbusplatform.io>
2021-10-20 13:04:40 +0300

    Добавил состояние и пример двухстороннего связывания

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-    let likes = 5;
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('Текст внутри инпута');
 
     function increment() {
-        likes += 1;
-        console.log(likes);
+        setLikes(likes + 1);
     }
 
     function decrement() {
-        likes -= 1;
-        console.log(likes);
+        setLikes(likes - 1);
     }
 
     return (
         <div className="App">
             <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input 
+                type="text" 
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
